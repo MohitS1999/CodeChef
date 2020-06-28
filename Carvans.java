917f2b06d6bf1de51f8e3855bbd74e04f9cf1b85
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-28 22:15:06 +0530

    Basic

---
+++ Carvans.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Carvans{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int c=1;
+			int n=sc.nextInt();
+			int x=sc.nextInt();
+			int m=x;
+			for(i=1;i<n;i++){
+				if(x<m){
+					m=x;
+					c++
+				}
+			}
+			System.out.println(c);
+		}
+	}
+}
