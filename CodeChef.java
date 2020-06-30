4cd5c302d9129e7472bcc10ace965c2c1c352408
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-06-30 22:47:30 +0530

    Basic

--- CodeChef.java
+++ CodeChef.java
@@ -3,6 +3,7 @@ class FullBarier{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt();
+		//testcases
 		while(t-->0){
 			int n=sc.nextInt();
 			int h=sc.nextInt();
