ecebda707766e2df7e158f8b2b92dc1c13e74380
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-07-11 23:21:38 +0530

    Basic

---
+++ ThePromised.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class ThePromised{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int M=sc.nextInt();
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int s=sc.nextInt();
+		int x[]=new int[X];
+		int y[]=new int[Y];
+		for(int i=0;i<X;i++){
+			x[i]=sc.nextInt();
+		}
+		for(int i=0;i<Y;i++){
+		y[i]=sc.nextInt();
+		}
+		int m=0,p=0,n1,n2,n3,n4;
+		for(int i=0;i<=Y;i++){
+			int n=0;
+			if(i<Y){
+				n1=y[i]-(m+1);
+				//System.out.println(m);
+			//	System.out.println("y[i]"+y[i]);
+				n3=n1%s;
+				n1=n1-n3;
+				//System.out.println(n1);
+				for(int j=0;j<=X;j++){
+					if(j<X){
+						n2=x[j]-(n+1);
+					//	System.out.println(n);
+					//	System.out.println(n2);
+						n4=(n2%s);
+						n2=n2-n4;
+						p+=n1*n2;
+					//	System.out.println("if y if x...."+p);
+						n=x[j];
+					}
+					else if(j==X){
+						n2=N-x[j-1];
+						n4=(n2%s);
+						n2=n2-n4;
+						//System.out.println(n2);
+						//System.out.println(p);
+					//	System.out.println(n1);
+						p+=n1*n2;
+						//System.out.println("if y elseif x...."+p);
+					}
+				}
+				m=y[i];
+			}
+			else if(i==Y){
+				n1=M-y[i-1];
+				//System.out.println("--"+n1);
+				n3=(n1%s);
+				n1=n1-n3;
+				//System.out.println(n1);
+				for(int j=0;j<=X;j++){
+					if(j<X){
+						n2=x[j]-(n+1);
+						n4=n2%s;
+						n2=n2-n4;
+						p+=n1*n2;
+						//System.out.println("elseif if x...."+p);
+						n=x[j];
+					}
+					else if(j==X){
+						n2=N-x[j-1];
+						n4=(n2%s);
+						n2=n2-n4;
+						p+=n1*n2;
+						//System.out.println("elseif if x...."+p);
+					}
+				}
+			}
+		}
+		//System.out.println(p);
+		p=p/(s*s);
+		System.out.println(p);
+	}
+}
